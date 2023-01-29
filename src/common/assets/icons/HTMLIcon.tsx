3dbe500257578a688dd73b76e24370bf13effaed
Oleg <shevchenko.oleg.87@gmail.com>
2023-01-29 13:22:27 +0300

    refactor skill card for mobile version

--- src/common/assets/icons/HTMLIcon.tsx
+++ src/common/assets/icons/HTMLIcon.tsx
@@ -2,7 +2,7 @@ import React, {SVGProps} from 'react'
 
 const HtmlIcon = (props: SVGProps<SVGSVGElement>) => {
     return (
-        <svg height="110px" width="110px" viewBox="4 5 48 48" xmlns="http://www.w3.org/2000/svg" {...props}>
+        <svg height="110px" width="110px" viewBox="4 4 48 48" xmlns="http://www.w3.org/2000/svg" {...props}>
             <path d="M41,5H7l3,34l14,4l14-4L41,5L41,5z" fill="#E65100"/>
             <path d="M24 8L24 39.9 35.2 36.7 37.7 8z" fill="#FF6D00"/>
             <path d="M24,25v-4h8.6l-0.7,11.5L24,35.1v-4.2l4.1-1.4l0.3-4.5H24z M32.9,17l0.3-4H24v4H32.9z" fill="#FFF"/>
--- src/component/Skills/Skill/Skill.tsx
+++ src/component/Skills/Skill/Skill.tsx
@@ -3,6 +3,7 @@ import HeroiconsOutlineArrowRight from "../../../common/assets/icons/HeroiconsOu
 import { motion } from 'framer-motion';
 import {viewportSettingsType} from "../../../common/tools/types";
 import {viewportDelayDeterminate} from "../../../common/tools/viewportDelayDeterminate";
+import s from './Skill.module.css'
 
 type SkillPropsType = {
     title: string
@@ -45,20 +46,39 @@ const Skill = forwardRef(({title, icon, description, link}: SkillPropsType, ref:
         window.open(link)
     }
 
-    return (
-        <motion.div initial='hidden' whileInView='visible' viewport={{amount:viewportDelayDeterminate(document.documentElement.clientWidth, viewportSettings), once: true}}
-                    variants={imageAnimation} transition={{duration:1.5}} ref={ref}
-                    className=' relative flex flex-col w-[270px] items-center justify-start h-[450px] bg-background-white rounded-2xl mb-[50px] mr-[20px]'>
-            <div className='w-[90px] h-[90px] mt-[20px]'>{icon}</div>
-            <h3 className='text-[24px] mt-[10px] mb-[20px]'>{title}</h3>
-            <span className='text-[15px] mb-[15px] font-light px-[20px] font-description text-center text-justify'>{description}</span>
-            <div className='absolute bottom-0 flex justify-between text-[14px] font-light border-t-[1px] border-background-gray-light w-full rounded-b-2xl
+    if (document.documentElement.clientWidth > 700){
+        return (
+            <motion.div initial='hidden' whileInView='visible' viewport={{amount:viewportDelayDeterminate(document.documentElement.clientWidth, viewportSettings), once: true}}
+                        variants={imageAnimation} transition={{duration:1.5}} ref={ref}
+                        className=' relative flex flex-col w-[270px] items-center justify-start h-[450px] bg-background-white rounded-2xl mb-[50px] mx-[20px]'>
+                <div className='w-[90px] h-[90px] mt-[20px]'>{icon}</div>
+                <h3 className='text-[24px] mt-[10px] mb-[20px]'>{title}</h3>
+                <span className='text-[15px] mb-[15px] font-light px-[20px] font-description text-center text-justify'>{description}</span>
+                <div className='absolute bottom-0 flex justify-between text-[14px] font-light border-t-[1px] border-background-gray-light w-full rounded-b-2xl
             px-[25px] h-[40px] pt-[8px] duration-700 hover:bg-black  hover:text-white' onMouseOver={onMouseOverHandler} onMouseLeave={onMouseLeave} onClick={onReadMoreClickHandler}>
-                READ MORE
-                <HeroiconsOutlineArrowRight color={over ? 'white' : 'black'}/>
-            </div>
-        </motion.div>
-    );
+                    READ MORE
+                    <HeroiconsOutlineArrowRight color={over ? 'white' : 'black'}/>
+                </div>
+            </motion.div>
+        );
+    } else {
+        return (
+            <motion.div initial='hidden' whileInView='visible' viewport={{amount:viewportDelayDeterminate(document.documentElement.clientWidth, viewportSettings), once: true}}
+                        variants={imageAnimation} transition={{duration:1.5}} ref={ref}
+                        className={`${s.skillBlockMobile} ${over ? s.mobileActive : ''}`}
+                        onMouseOver={onMouseOverHandler} onMouseLeave={onMouseLeave}>
+                <div className='w-[90px] h-[90px]'>{icon}</div>
+                {/*<h3 className='text-[24px] mt-[10px] mb-[20px]'>{title}</h3>*/}
+                {/*<span className='text-[15px] mb-[15px] font-light px-[20px] font-description text-center text-justify'>{description}</span>*/}
+            {/*    <div className='absolute bottom-0 flex justify-between text-[14px] font-light border-t-[1px] border-background-gray-light w-full rounded-b-2xl*/}
+            {/*px-[25px] h-[40px] pt-[8px] duration-700 hover:bg-black  hover:text-white' onMouseOver={onMouseOverHandler} onMouseLeave={onMouseLeave} onClick={onReadMoreClickHandler}>*/}
+            {/*        READ MORE*/}
+            {/*        <HeroiconsOutlineArrowRight color={over ? 'white' : 'black'}/>*/}
+            {/*    </div>*/}
+            </motion.div>
+        )
+    }
+
 });
 
 export default Skill;
